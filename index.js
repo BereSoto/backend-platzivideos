b18789f8b9a9421d08c467856dfdd2e65a1475ae
BereSoto <beresoto.f@gmail.com>
2019-11-05 13:48:26 -0600

    middleware

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 
+const { logErrors, errorHandler } = require('utils/middleware/errorHandlers.js');
+
 app.use(express.json());
+app.use(logErrors);
+app.use(errorHandler);
 
 moviesApi(app);
 
